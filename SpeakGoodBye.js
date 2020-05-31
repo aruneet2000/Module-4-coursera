48ad32de1705def1d9ec7bbec7e0acb6de6e698d
Aruneet Roychowdhury <aruneet2015@gmail.com>
2020-05-31 16:02:02 +0530

    assignment done

---
+++ SpeakGoodBye.js
@@ -0,0 +1,15 @@
+
+const byeSpeaker ={
+    
+      speakWord : "Good Bye",
+
+};
+
+
+(function () {
+    byeSpeaker.speak = function (userName) {
+        return (byeSpeaker.speakWord + " " + userName);
+    }
+}());
+
+
---
+++ SpeakHello.js
@@ -0,0 +1,17 @@
+
+ const helloSpeaker ={
+
+      speakWord : "Hello",
+
+ };
+
+
+ (function () {
+    helloSpeaker.speak = function (userName) {
+        return (helloSpeaker.speakWord + " " + userName);
+    }
+}());
+
+
+
+
